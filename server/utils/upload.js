d3d0229a21f6f6f4f0db61466cc45a6c79b47722
AbdulrahmanBassem <abdulrahmanbassem@outlook.com>
2026-01-24 15:15:00 +0200

    fix: update image upload handling to use file paths instead of filenames

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,28 +1,51 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${Date.now()}-${file.originalname.replace(/\s+/g, "-")}`);
+//   },
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith("image/")) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Only images are allowed"), false);
+//   }
+// };
+
+// const upload = multer({ 
+//   storage, 
+//   fileFilter,
+//   limits: { fileSize: 10 * 1024 * 1024 } // 10MB limit
+// });
+
+// module.exports = { upload };
+
+
 const multer = require("multer");
+const { v2: cloudinary } = require("cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname.replace(/\s+/g, "-")}`);
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "sync-social-media", 
+    allowed_formats: ["jpg", "png", "jpeg", "webp", "HEIC"], 
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/")) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only images are allowed"), false);
-  }
-};
-
-const upload = multer({ 
-  storage, 
-  fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 } // 10MB limit
-});
+const upload = multer({ storage: storage });
 
 module.exports = { upload };
-
